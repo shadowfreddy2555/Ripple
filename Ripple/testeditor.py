d4322920a8be0ead8f657b02398bbfda08701350
shadowfreddy2555 <124621102+shadowfreddy2555@users.noreply.github.com>
2023-11-20 20:31:46 +0100

    Added more colors to cubes and added dark mode

--- Ripple/testeditor.py
+++ Ripple/testeditor.py
@@ -1,11 +1,10 @@
 import tkinter as tk
-import os
-import sys
+import tkinter.colorchooser as cc
 
 def create_cube(event):
     if not is_dragging.get():
         x, y = event.x, event.y
-        cube = canvas.create_rectangle(x-15, y-15, x+15, y+15, fill="red", outline="", tags="cube")
+        cube = canvas.create_rectangle(x-15, y-15, x+15, y+15, fill=current_color, outline="", tags="cube")
         cubes.append(cube)
 
 def drag_start(event):
@@ -20,22 +19,23 @@ def drag_start(event):
 
 def snap_to_cube():
     global selected_cube, cubes
-    x1, y1, x2, y2 = canvas.bbox(selected_cube) # type: ignore
-    mid_y = (y1 + y2) / 2
-    snap_y = None
-    min_dist = float('inf')
-    for cube in cubes:
-        if cube != selected_cube:
-            cx1, cy1, cx2, cy2 = canvas.bbox(cube)
-            mid_cx = (cx1 + cx2) / 2
-            mid_cy = (cy1 + cy2) / 2
-            dist_x = abs((x1 + x2) / 2 - mid_cx)
-            dist_y = abs(mid_y - mid_cy)
-            if dist_x <= 150 and dist_y <= 1 and dist_y < min_dist:
-                min_dist = dist_y
-                snap_y = mid_cy
-    if snap_y is not None:
-        canvas.move(selected_cube, 0, snap_y - mid_y)
+    if selected_cube in cubes:
+        x1, y1, x2, y2 = canvas.bbox(selected_cube)
+        mid_y = (y1 + y2) / 2
+        snap_y = None
+        min_dist = float('inf')
+        for cube in cubes:
+            if cube != selected_cube:
+                cx1, cy1, cx2, cy2 = canvas.bbox(cube)
+                mid_cx = (cx1 + cx2) / 2
+                mid_cy = (cy1 + cy2) / 2
+                dist_x = abs((x1 + x2) / 2 - mid_cx)
+                dist_y = abs(mid_y - mid_cy)
+                if dist_x <= 150 and dist_y <= 1 and dist_y < min_dist:
+                    min_dist = dist_y
+                    snap_y = mid_cy
+        if snap_y is not None:
+            canvas.move(selected_cube, 0, snap_y - mid_y)
 
 def drag_motion(event):
     global prev_x, prev_y
@@ -52,12 +52,41 @@ def drag_stop(event):
 
 def delete_cube(event):
     canvas.delete(tk.CURRENT)
+    cubes.pop()  # Remove the last cube from the list
 
 def leave_game():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.system(f"python {script_dir}/player.py")
     root.destroy()
     sys.exit()
+
+def change_color():
+    global current_color
+    color = cc.askcolor()[1]  # Returns the chosen color as a hex value
+    if color:
+        current_color = color
+
+def select_color():
+    global current_color
+    color = cc.askcolor(color=current_color)[1]  # Returns the chosen color as a hex value
+    if color:
+        current_color = color
+
+def toggle_background():
+    global is_dark_mode
+    if is_dark_mode:
+        root.config(bg="white")
+        canvas.config(bg="white")
+        is_dark_mode = False
+    else:
+        root.config(bg="black")
+        canvas.config(bg="black")
+        is_dark_mode = True
+
+def undo(event):
+    if cubes:
+        canvas.delete(cubes[-1])
+        cubes.pop()
 
 root = tk.Tk()
 root.title("Cube Creator")
@@ -75,16 +104,25 @@ block_button.pack(side=tk.LEFT)
 leave_button = tk.Button(button_frame, text="Leave Game", command=leave_game)
 leave_button.pack(side=tk.LEFT)
 
+color_button = tk.Button(button_frame, text="Change Color", command=select_color)
+color_button.pack(side=tk.LEFT)
+
+dark_button = tk.Button(button_frame, text="Dark", command=toggle_background)
+dark_button.pack(side=tk.LEFT)
+
 is_dragging = tk.BooleanVar()
 is_dragging.set(False)
 selected_cube = None
 prev_x, prev_y = 0, 0
 cubes = []
+current_color = "red"  # Default color
+is_dark_mode = False  # Flag for dark mode
 
 block_button.bind("<Button-1>", lambda event: canvas.bind("<Button-1>", create_cube))
 canvas.tag_bind("cube", "<ButtonPress-1>", drag_start)
 canvas.bind("<B1-Motion>", drag_motion)
 canvas.bind("<ButtonRelease-1>", drag_stop)
 canvas.bind("<Button-3>", delete_cube)
+root.bind("<Control-z>", undo)  # Bind Ctrl + Z to the undo function
 
 root.mainloop()
